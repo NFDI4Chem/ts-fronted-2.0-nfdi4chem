25d99c6e5b48887861781ec09c60d51c3b917d91
renaissancenerd <vatsallimbachiya@gmail.com>
2021-08-03 15:44:51 +0200

    added nfdi4chem logo

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -9,7 +9,7 @@ function Header () {
     <section className="header">
       <section className="header-top">
         <section className="header-top__logo">
-          <a href="/" className="header-logo">LOGO</a>
+          <img src={"https://nfdi4chem.de/wp-content/uploads/2019/01/cropped-Rastergrafik2-5.png"} alt="nfdi4chem logo" height={100} width={180} />
         </section>
         <section className="header-top__navbar">
           <section className="header-top__navigation">
