f7c8c19cf179c0d12730362fc103ab186a5c17b2
renaissancenerd <vatsallimbachiya@gmail.com>
2021-08-05 15:12:11 +0200

    adding content below searchbar

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,6 +9,11 @@ class Home extends Component{
         <div id="mainpageSearchBox">
            <h4>Welcome to NFDI4Chem Terminology Service</h4>
            <SearchBar />
+           <h4>About NFDI4Chem TS</h4>
+           <p class="about">The NFDI4Chem Terminology Service is a repository for chemistry and related ontologies providing a single point of access to the latest ontology versions. 
+              You can browse or search the ontologies and look into their terms and relations. The Terminology Service can be used either by humans throught the website or by machines via the TS API.
+              The NFDI4Chem Terminology Service is developed and maintained by TIB - Leibniz Information Centre for Science and Technology. 
+              It is part of the service portfolio of the NFDI4Chem consortium within the National Research Data Infrastructure.</p>
         </div>
         );
     }
