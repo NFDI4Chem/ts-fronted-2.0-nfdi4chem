1106730489a5b700a607ca69b29e3ab26c5c1eb7
renaissancenerd <vatsallimbachiya@gmail.com>
2021-08-06 10:37:15 +0200

    added content

--- src/components/documentation/Documentation.jsx
+++ src/components/documentation/Documentation.jsx
@@ -4,10 +4,22 @@ import React from "react";
 class Documentation extends Component{
     render(){
         return(
-        <div>
-           <h3>Documentation</h3>
-           <p>Add content</p>
-        </div>
+            <div id="content">
+              <div class="sect1">
+              <h3 id="_about_the_terminology_service">How to use the NFDI4Chem Terminology website</h3>
+                <div class="sectionbody">
+                  <div class="paragraph">
+                     <p>There are two main ways of browsing the Terminology Service. You can either browse the ontology list available via the ontology menu. You enter the ontology on top level and browse through the tree view to lower levels or you search by a specific term. For this you can use the search box to enter the term that you are looking for. The provided results show all ontologies where the term was found. By using the advanced search, you can further restrict the results to one or multiple ontologies or to a specific 'type'. These types can be class, property or individual. Futhermore, you can restrict searches to one or more ontology. No matter what way of browsing an ontology you choose, your search leads you to the view of a term.</p>
+                  </div>
+              <h3>How to use the NFDI4Chem Terminology Service's API</h3>
+                <div class="paragraph">
+                   <p><strong><a href="../docs/api">API documentation</a>:</strong><br/>
+                     Documentation on how to use the Terminology Service&#8217;s REST API</p>
+                   <p>Publicly available API commands can also be analyzed and executed from the <strong><a href="http://service.tib.eu/ts4tib/swagger-ui.html">Swagger Documentation</a></strong>. The underlying models can further be viewed through this documentation for a deeper understanding of the API commands.</p>
+                </div>
+                </div>
+              </div>
+            </div>
         );
     }
 }
--- src/components/help/Help.jsx
+++ src/components/help/Help.jsx
@@ -1,13 +1,26 @@
 import { Component } from "react";
 import React from "react";
+import './Help.css'
 
 class Help extends Component{
     render(){
         return(
-        <div>
-           <h3>Help</h3>
-           <p>Add content</p>
-        </div>
+            <div id="content">
+            <h4>Help</h4>
+            <div class="paragraph">
+              <p>For feedback, enquiries or suggestion about NFDI4Chem Terminology Service or to request a new ontology please contact our NFDI4Chem helpdesk.</p>
+            </div>
+            <div class="paragraph">
+              <p>For announcements relating to our NFDI4Chem project such as new services or events, please sign up to our <a href="https://lists.nfdi.de/postorius/lists/nfdi4chem-announce.lists.nfdi.de/">NFDI4Chem newsletter</a></p>
+            </div>
+            <div class="paragraph">
+              <p>Looking for additional information or help on research data management, data repositories or electronic lab notebooks (ELN) for chemistry? Visit our <a href="https://nfdi4chem.de/">NFDI4Chem Website</a></p>
+            </div>
+            <h4 id="How-to-get-in-touch">NFDI4Chem Helpdesk</h4>
+            <div class="paragraph">
+              <p>Either send an email to <a href="mailto:helpdesk@nfdi4chem.de">helpdesk@nfdi4chem.de</a> or use the Helpdesk <a href="https://nfdi4chem.de/index.php/helpdesk/">contact form</a></p>
+            </div>
+          </div>
         );
     }
 }
