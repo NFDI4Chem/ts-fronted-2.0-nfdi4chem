33a90ebfb1db8f4d06c86fc1100c5e0dcc52215b
renaissancenerd <vatsallimbachiya@gmail.com>
2021-12-21 14:22:13 +0100

    adding new logo

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -9,7 +9,7 @@ function Header () {
     <section className="header">
       <section className="header-top">
         <section className="header-top__logo">
-          <img src={"https://nfdi4chem.de/wp-content/uploads/2019/01/cropped-Rastergrafik2-5.png"} alt="nfdi4chem logo" height={100} width={180} />
+          <img src={"../assets/img/new-nfdi4chemlogo.png"} alt="nfdi4chem logo" height={100} width={180} />
         </section>
         <section className="header-top__navbar">
           <section className="header-top__navigation">
