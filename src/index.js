54ab88a6ebb32b35a7876abe98c3b8bc6ea634b5
renaissancenerd <vatsallimbachiya@gmail.com>
2022-01-24 17:04:32 +0100

    adding ontologydetail in route wrapper

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import OntologyPage from 'tib-ts-library/dist/ontologyDetail';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
+    <Router >
+      <Route exact path="/ontologies/:ontologyId" component={OntologyPage} />
+      <App/>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
