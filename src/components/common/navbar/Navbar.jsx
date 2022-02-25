9da064dcca2604f4107b85fd520f0145c0df1dd9
renaissancenerd <vatsallimbachiya@gmail.com>
2022-02-25 13:46:25 +0100

    adding variables for navigation

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 import './Navbar.css'
 
 function Navbar () {
+  const basename = process.env.REACT_APP_BASENAME || null;
 
   return (
+    <Router basename={basename}>
     <section className="navbar">
       <Link to="/" className="navbar-item">Home</Link>
       <Link to="/ontologies" className="navbar-item">Ontologies</Link>
@@ -13,6 +15,7 @@ function Navbar () {
       <Link to="/documentation" className="navbar-item">Documentation</Link>
       <Link to="/about" className="navbar-item">About</Link>
   </section>
+  </Router>
   )
 
 }
