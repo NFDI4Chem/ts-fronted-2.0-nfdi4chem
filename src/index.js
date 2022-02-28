1546c41b2f3c9909832e90d08053f8f2a4aeaca3
renaissancenerd <vatsallimbachiya@gmail.com>
2022-02-28 10:32:09 +0100

    removing previous route basename

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import OntologyPage from 'tib-ts-library/dist/ontologyDetail';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/ts" >
-      <Route exact path="/ontologies/:ontologyId" component={OntologyPage} />
       <App/>
-    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
