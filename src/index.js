00069b66682694f5e6c85715490eae8d41fa09b9
renaissancenerd <vatsallimbachiya@gmail.com>
2022-03-10 09:57:21 +0100

    adding basename env

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import './index.css';
 import App from './App';
 import OntologyPage from 'tib-ts-library/dist/ontologyDetail';
 
+const basename = process.env.REACT_APP_BASENAME || null;
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/ts" >
+    <Router basename={basename} >
       <Route exact path="/ontologies/:ontologyId" component={OntologyPage} />
       <App/>
     </Router>
