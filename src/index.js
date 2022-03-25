f939efd0c4229adcbbd7be0c7d282a04c33199a1
renaissancenerd <vatsallimbachiya@gmail.com>
2022-03-25 12:59:24 +0100

    changing routes

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import OntologyPage from 'tib-ts-library/dist/ontologyDetail';
+import OntologyPage from './components/ontologies/OntologyDetail'
 
-const basename = process.env.REACT_APP_BASENAME || null;
+const basename = process.env.BASENAME ;
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={basename} >
-      <Route exact path="/ontologies/:ontologyId" component={OntologyPage} />
+    <Router basename={basename}>
+       <Route exact path="/ontologies/:ontologyId" component={OntologyPage} />
       <App/>
     </Router>
   </React.StrictMode>,
